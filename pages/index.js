4ec6cc69ebe03149f53fa961ce886205de96e583
João Victor <joaovictorca2004@gmail.com>
2024-02-21 23:54:37 +0000

    adiciona scripts  e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Se eu te mandei esse link, saiba que eu TE AMO! 😎</h1>
+  return <h1>Se eu te mandei esse link, saiba que eu TE AMO! 😎</h1>;
 }
 
 export default Home;
