be95b04a1dc899bcb246cabc66a0f90214e18bb4
joaovic-wsl <joaovictorca2004@gmail.com>
2024-02-22 18:59:42 -0300

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Disciplina é a chave para alcançar objetivos extraordinários... <br />Porque se não houver disciplina, o que mais pode te levar ao sucesso?! 😎</h1>;
+  return (
+    <h1>
+      Disciplina é a chave para alcançar objetivos extraordinários... <br />
+      Porque se não houver disciplina, o que mais pode te levar ao sucesso?! 😎
+    </h1>
+  );
 }
 
 export default Home;
