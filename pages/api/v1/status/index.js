4846b15df71e40f54df60220d3254cf4548da3c0
joaovic <joaovictorca2004@gmail.com>
2025-12-04 12:14:22 -0300

    add `database.js` and `.end`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function status(req, res) {
+import database from '../../../../infra/database.js';
+
+export default async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
   const status = {
     status: res.statusCode,
     message: 'alunos do curso.dev são pessoas acima da média'
