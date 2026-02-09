104794846fc923317d21f90754e9663dbc5afdd3
joaovic <joaovictorca2004@gmail.com>
2026-02-09 17:26:10 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,30 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  const status = {
-    status: res.statusCode,
-    message: 'alunos do curso.dev são pessoas acima da média'
-  };
+  const updateAt = new Date().toISOString();
 
-  res.json(status);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const showMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(showMaxConnections.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const selectOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionsValue = selectOpenedConnections.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnections,
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
