652dd33638496b3930342e45b930bf954a2c3b0e
João Victor C. Alves <joaovictorca2004@gmail.com>
2026-05-04 20:33:35 -0300

    fix `/migrations` endpoint connection bug

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -14,11 +14,4 @@ test("GET to /api/v1/migrations should return 200", async () => {
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
-
-  expect(process.env.NODE_ENV).toEqual("test");
-  expect(process.env.POSTGRES_HOST).toBeDefined();
-  expect(process.env.POSTGRES_PORT).toBeDefined();
-  expect(process.env.POSTGRES_USER).toBeDefined();
-  expect(process.env.POSTGRES_DB).toBeDefined();
-  expect(process.env.POSTGRES_PASSWORD).toBeDefined();
 });
